e8c0125c6a1f1534c4821ff36c69130e0db0e69b
sidhantkumar <sd247182@dal.ca>
2025-09-04 10:52:05 -0300

    feat: added function to calculate w and b for gradient descent.

--- house_price_ml.py
+++ house_price_ml.py
@@ -17,3 +17,25 @@ def compute_cost(X, y, w, b):
     total_cost /= (2*m)
 
     return total_cost
+
+# this function computes w and b for gradient descent
+def compute_gradient(X, y, w, b):
+    "X: Features array"
+    "y: Target(Actual) values"
+    "w: parameter"
+    "b: parameter"
+
+    m = X.shape[0]
+
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = np.dot(w, X[i]) + b
+        dj_dw += (f_wb - y[i]) * X[i]
+        dj_db += (f_wb - y[i])
+
+    dj_dw /= m
+    dj_db /= m
+
+    return dj_dw, dj_db
